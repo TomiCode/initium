9e90658fa54d59b9e3f01b98b17fb452e8e4ea9d
Tomasz Król <tomicode@gmail.com>
2017-09-29 00:12:01 +0200

    Load templates into memory.

--- app/template.go
+++ app/template.go
@@ -7,7 +7,7 @@ import "path/filepath"
 import "html/template"
 
 // App root template pointer.
-var appTemplates *template.Template
+var appTemplate *template.Template
 
 // Register the template directory.
 func (app *Initium) SetTemplateDir(dir string) {
@@ -21,11 +21,23 @@ func registerTemplate(path string, file os.FileInfo, err error) error {
   if file.IsDir() || !strings.Contains(path, ".tmpl") {
     return nil
   }
-
   var end, start = strings.Index(path, "."), strings.Index(path, "/") + 1
   var alias = strings.Replace(path[start:end], "/", ".", -1)
 
   log.Println("Registering template alias:", alias)
+  if appTemplate == nil {
+    appTemplate, err = template.New(alias).Funcs(templateFuncs()).ParseFiles(path)
+  } else {
+    _, err = appTemplate.New(alias).ParseFiles(path)
+  }
 
+  if err != nil {
+    log.Println("Parsing error:", err)
+  }
   return nil
+}
+
+// Template function mapping for the whole application.
+func templateFuncs() template.FuncMap {
+  return template.FuncMap{}
 }
